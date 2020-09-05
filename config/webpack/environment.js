99cce5f1dd013a2464f6ef55cb1d1d7ce2b205c9
m-teraoka-mac <mai.teraoka2525@gmail.com>
2020-09-05 19:52:26 +0900

    init

---
+++ config/webpack/environment.js
@@ -0,0 +1,28 @@
+const { environment } = require('@rails/webpacker')
+const { VueLoaderPlugin } = require('vue-loader')
+const vue = require('./loaders/vue')
+
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+environment.loaders.prepend('vue', vue)
+
+// Vue.js フル版（Compiler入り）
+environment.config.resolve.alias = { 'vue$': 'vue/dist/vue.esm.js' }
+
+// const webpack = require('webpack')
+// environment.plugins.prepend('Provide',
+//     new webpack.ProvidePlugin({
+//         $: 'jquery/src/jquery',
+//         jQuery: 'jquery/src/jquery'
+//     })
+// )
+const webpack = require('webpack')
+environment.plugins.prepend(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: 'popper.js'
+  })
+)
+
+module.exports = environment
